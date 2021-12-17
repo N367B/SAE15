143900cf65fa741afa73656a66b9057c076232a8
Noe <bodinoe85@gmail.com>
2021-12-17 09:38:08 +0100

    Premères modfis

--- pokestats_Pokemon.py
+++ pokestats_Pokemon.py
@@ -1,6 +1,18 @@
 import markdown
 import pokepy
 
-def test(n):
-	poekmon = pokepy.V2Client().get_pokemon(n)
-	print(pokemon.weight)
+def getPokemon(n):
+	pokemon = pokepy.V2Client().get_pokemon(n)
+	return pokemon
+
+def statsGeneral(pokemon):
+	return pokemon.types[0].type.name
+def mardownStatsGeneral(stats):
+	print('ok')
+
+def downloadPokemonSprite(n):
+	print('ok')
+def mardownToHTML(file):
+	print('ok')
+
+print(statsGeneral(getPokemon(56)))
