e737c9a2a457a20c3580b905b22121857d89f078
Armand <armand.lemee@etu.unicaen.fr>
2022-01-05 11:34:45 +0100

    jlfhgsdrhkgjkdsgjkdbvh

--- pokestats_heightHP.py
+++ pokestats_heightHP.py
@@ -8,24 +8,27 @@ def getPokemonList(first, last) :
 	return pokemons
 
 def statsHeight(pokemonList) :
+	nom = [None] * len(pokemonList)
 	taille = [None] * len(pokemonList)
 	vie = [None] * len(pokemonList)
-	nom = [None] * len(pokemonList)
+	infos = [None] * len(pokemonList)
 	for i in range(len(pokemonList)) :
 		nom[i] = pokemonList[i].name
 		taille[i] = pokemonList[i].height
 		vie[i] = pokemonList[i].stats[0].base_stat
-	return [nom, taille, vie]
+		infos[i] = [nom[i], taille[i], vie[i]]
+	infos.sort(key = lambda x: x[1])
+	return infos
 
 
 def markdownStatsHeightHP(stats) :
 	markdown = "#Les pokémon grands sont-ils les plus tanky ? \n"
 	markdown += "| Nom | Taille | HP | \n"
-	for i in range(len(stats[0])) :
+	for i in range(len(stats)) :
 		markdown += "| | | | \n"
-		markdown += "| " + str(stats[0][i])+ " | " + str(stats[1][i])+ " | " + str(stats[2][i])+ " |" + "\n"
+		markdown += "| " + str(stats[i][0])+ " | " + str(stats[i][1])+ " | " + str(stats[i][2])+ " |" + "\n"
 	with open ("./markdown/StatsHeightHP/statsHeightHP.md", 'w') as handler:
 		handler.write(markdown)
 
 
-markdownStatsHeightHP(statsHeight(getPokemonList(1, 5)))
+markdownStatsHeightHP(statsHeight(getPokemonList(1, 10)))
