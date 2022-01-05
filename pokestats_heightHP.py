23b00ebc97ee6008932549d035bd792f5781e06f
Armand <armand.lemee@etu.unicaen.fr>
2022-01-05 10:42:36 +0100

    Oui

--- pokestats_heightHP.py
+++ pokestats_heightHP.py
@@ -1,4 +1,4 @@
-import markdown 
+import markdown
 import pokepy
 
 def getPokemonList(first, last) :
@@ -6,4 +6,26 @@ def getPokemonList(first, last) :
 	for i in range(first, last+1):
 		pokemons.append(pokepy.V2Client().get_pokemon(i))
 	return pokemons
-print(getPokemonList(1, 5))
+
+def statsHeight(pokemonList) :
+	taille = [None] * len(pokemonList)
+	vie = [None] * len(pokemonList)
+	nom = [None] * len(pokemonList)
+	for i in range(len(pokemonList)) :
+		nom[i] = pokemonList[i].name
+		taille[i] = pokemonList[i].height
+		vie[i] = pokemonList[i].stats[0].base_stat
+	return [nom, taille, vie]
+
+
+def markdownStatsHeightHP(stats) :
+	markdown = "#Les pokémon grands sont-ils les plus tanky ? \n"
+	markdown += "| Nom | Taille | HP | \n"
+	for i in range(len(stats[0])) :
+		markdown += "| | | | \n"
+		markdown += "| " + str(stats[0][i])+ " | " + str(stats[1][i])+ " | " + str(stats[2][i])+ " |" + "\n"
+	with open ("./markdown/StatsHeightHP/statsHeightHP.md", 'w') as handler:
+		handler.write(markdown)
+
+
+markdownStatsHeightHP(statsHeight(getPokemonList(1, 5)))
